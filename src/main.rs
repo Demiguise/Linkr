46f202dfbf18f007ce50edbe00853fb243c5c732
Andy <andy.c.hewitson@gmail.com>
2020-02-07 19:02:16 +0100

    Added very basic reading parsing of cfg

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
+extern crate yaml_rust;
+use yaml_rust::YamlLoader;
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main()
 {
-  println!("Hello, world!");
+    let mut file = File::open(".linkr").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    let cfg = YamlLoader::load_from_str(&contents).unwrap();
+
+    println!("{:?}", cfg);
 }
