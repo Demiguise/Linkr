f7950c07ae5750d7bc6fa428df60e2fe09deeb4c
Andy <andy.c.hewitson@gmail.com>
2020-02-07 20:57:32 +0100

    Added error chaining. Added run function. Made error handling work!

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,64 @@
 extern crate yaml_rust;
-use yaml_rust::YamlLoader;
+#[macro_use]
+extern crate error_chain;
+use yaml_rust::{Yaml, YamlLoader};
 use std::fs::File;
 use std::io::prelude::*;
 
-fn load_config(filename: &str) -> Result<Vec<yaml_rust::Yaml>, yaml_rust::ScanError>
-{
-    let mut file = File::open(filename).unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
+mod errors {
+    error_chain! { }
+}
 
-    YamlLoader::load_from_str(&contents)
+use errors::*;
+
+fn load_config(filename: &str) -> Result<Option<Yaml>>
+{
+    let mut file = File::open(filename)
+        .chain_err(|| "Unable to open file")?;
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .chain_err(|| "Unable to read contents of file")?;
+
+    let data = YamlLoader::load_from_str(&contents)
+        .chain_err(|| "Failed to parse conents as YAML")?;
+
+    Ok(data.into_iter().nth(0))
 }
 
 fn main()
 {
-    let cfg = load_config(".linkr").unwrap();
-    println!("{:?}", cfg);
+    if let Err(ref e) = run()
+    {
+        let stderr = &mut ::std::io::stderr();
+        let errmsg = "Error writing to stderr";
+
+        writeln!(stderr, "error: {}", e).expect(errmsg);
+
+        for e in e.iter().skip(1)
+        {
+            writeln!(stderr, "caused by: {}", e).expect(errmsg);
+        }
+
+        // The backtrace is not always generated. Try to run this example
+        // with `RUST_BACKTRACE=1`.
+        if let Some(backtrace) = e.backtrace()
+        {
+            writeln!(stderr, "backtrace: {:?}", backtrace).expect(errmsg);
+        }
+
+        ::std::process::exit(1);
+    }
+}
+
+fn run() -> Result<()>
+{
+    let cfg = load_config(".linkr")?;
+
+    for block in cfg
+    {
+        println!("{:?}\n---", block);
+    }
+
+    Ok(())
 }
